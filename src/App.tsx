834a86e4917335ba4a82a4713afb0c31f29b9a63
unknown <agsriyanto2@gmail.com>
2025-01-10 02:36:28 +0700

    slicing ui, navigation and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
+
 import reactLogo from './assets/printing.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,10 +9,37 @@ import Menu from './components/menu';
 import Home from './pages/home';
 import Request from './pages/request';
 import Invoice from './pages/invoice';
+import ItPeriperal from './pages/itPeriperal';
+import Sidebar from './components/sidebar';
+import ModalFinancial from './components/modalFinancial';
+import Maintenance from './pages/maintenance';
 
 function App() {
   const [count, setCount] = useState(0);
   const [menu, setMenu] = useState<string>('home');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const [cartItems, setCartItems] = useState([
+    {
+      id: 1,
+      name: "Thinkpad Ryzen 5 Pro",
+      price: 10000000,
+      vendor: "PT MBUI",
+      quantity: 1,
+      image: "https://via.placeholder.com/50", // Replace with your image URL
+    },
+    {
+      id: 2,
+      name: "Asus Charger",
+      price: 100000,
+      vendor: "PT MBUI",
+      quantity: 1,
+      image: "https://via.placeholder.com/50", // Replace with your image URL
+    },
+  ]);
 
   const renderMenu = () => {
     switch (menu) {
@@ -27,13 +56,32 @@ function App() {
 
   const handleChangeMenu = (menu: string) => {
     setMenu(menu);
+    navigate("/");
+  };
+
+  const props = {
+    isOpen,
+    setIsOpen,
+    cartItems,
+    setCartItems,
+    isModalVisible,
+    setIsModalVisible,
   };
 
   return (
     <>
-      <Navbar />
+      <Navbar {...props} />
       <Menu handleChange={handleChangeMenu} menu={menu} />
-      {renderMenu()}
+      <Sidebar {...props} />
+      <ModalFinancial isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} />
+
+      <Routes>
+        <Route path="/" element={renderMenu()} />
+        <Route path="/it-periperal" element={<ItPeriperal />} />
+        <Route path="/printing" element={<Maintenance />} />
+        <Route path="/stationary-supply" element={<Maintenance />} />
+        <Route path="/merchandise" element={<Maintenance />} />
+      </Routes>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/cardContent/index.tsx
@@ -0,0 +1,34 @@
+import { IconShoppingCart } from '@tabler/icons-react';
+
+import "./cardContent.scss";
+import iifLogo from '../../assets/iif-logo.svg';
+import formatNumber from '../../lib/utils';
+
+const CardContent = (props: any) => {
+  const { data } = props;
+
+  return (
+    <div className="card-content" >
+      <div className="card-content-title">
+        <div className="card-content-title-name">
+          <p className="category">{data?.category ?? ''}</p>
+          <p className="name">{data?.name ?? ''}</p>
+          <p className="price">IDR {formatNumber(data?.price ?? 0)}</p>
+        </div>
+        <div className="card-content-title-icon">
+          <IconShoppingCart size={20} color="#F58220" stroke="1" />
+        </div>
+      </div>
+      <div className="card-content-image">
+        <img src={data?.image ?? ''} alt="product" />
+      </div>
+      <div className="title-vendor">
+        <p className="title-vendor-1">Vendor Name</p>
+        <p className="title-vendor-2">{data?.vendor ?? ''}</p>
+      </div>
+      <div></div>
+    </div>
+  )
+};
+
+export default CardContent;
